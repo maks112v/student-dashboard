11b4a928a086e59422b62894b09d1adf9f4182d7
jeremiahtenbrink <jeremiahtenbrink@gmail.com>
2019-06-01 13:25:07 -0600

    Removed Function Component.

--- src/Views/Retro.jsx
+++ src/Views/Retro.jsx
@@ -55,155 +55,157 @@ class Retro extends React.Component{
     };
     
     render(){
-        return ( <Layout>
-            <Layout.Content
-                style={ { minHeight: "100vh", margin: "20px 10px" } }>
-                <Card
-                    style={ { maxWidth: "600px", margin: "20px auto" } }
-                    title="Daily Standup"
-                    actions={ [
-                        <Icon
-                            type="arrow-left"
-                            onClick={ () => this.props.history.push( "/" ) }
-                        />
-                    ] }
-                >
-                    <p>
-                        Standup is an important part of everyday life as a
-                        developer.
-                        During your team standups, you'll give a status
-                        update, and note
-                        anything blocking you from making progress.
-                    </p>
-                    <p>
-                        Your standup report at Lambda School is similar.
-                        Your responses
-                        should be transparent about your progress, and where
-                        you need
-                        help. You'll discuss your blockers in your Team
-                        Meetings.
-                    </p>
-                </Card>
-                <Row
-                    gutter={ 24 }
-                    style={ {
-                        maxWidth: "600px",
-                        margin: "20px auto",
-                        padding: "10px 20px"
-                    } }
-                >
-                    <MakeInput
-                        type="disabled"
-                        required
-                        value={ `${ this.props.user.firstName } ${ this.props.user.lastName }` }
-                        title="Student"
-                        desc="Your name will be auto filled"
-                    />
-                    <MakeInput
-                        type="suggest"
-                        required
-                        title="What did you study today?"
-                        name="studyToday"
-                        onChange={ select => this.setState( {
-                            studyToday: select
-                        } ) }
-                        value={ this.state.studyToday }
-                        desc="Use search to quickly find the module. Choose the one that matches the page you used in Training Kit"
-                        data={ this.props.lessons &&
-                        Object.values( this.props.lessons )
-                            .map( lesson => lesson ) }
-                    />
-                    <MakeInput
-                        type="disabled"
-                        required
-                        value="¯\_(ツ)_/¯"
-                        title="How are you feeling today?"
-                        desc="Please fill this out on Airtable we don't want to deal with unicode and emojis"
-                    />
-                    <MakeInput
-                        type="input"
-                        required
-                        name="projectSubmittion"
-                        onChange={ this.onChange }
-                        value={ this.state.projectSubmittion }
-                        title="What's the URL for your submission of today's project?"
-                        desc="To submit GitHub Pull Requests's, please include your specific pull request URL"
-                    />
-                    <MakeInput
-                        type="rate"
-                        required
-                        title="On a scale of 1-3, how would you rate your project submission?"
-                        value={ this.state.rateProject }
-                        onChange={ value => {
-                            this.setState( {
-                                rateProject: value
-                            } );
+        return (
+            
+            <Layout>
+                <Layout.Content
+                    style={ { minHeight: "100vh", margin: "20px 10px" } }>
+                    <Card
+                        style={ { maxWidth: "600px", margin: "20px auto" } }
+                        title="Daily Standup"
+                        actions={ [
+                            <Icon
+                                type="arrow-left"
+                                onClick={ () => this.props.history.push( "/" ) }
+                            />
+                        ] }
+                    >
+                        <p>
+                            Standup is an important part of everyday life as a
+                            developer.
+                            During your team standups, you'll give a status
+                            update, and note
+                            anything blocking you from making progress.
+                        </p>
+                        <p>
+                            Your standup report at Lambda School is similar.
+                            Your responses
+                            should be transparent about your progress, and where
+                            you need
+                            help. You'll discuss your blockers in your Team
+                            Meetings.
+                        </p>
+                    </Card>
+                    <Row
+                        gutter={ 24 }
+                        style={ {
+                            maxWidth: "600px",
+                            margin: "20px auto",
+                            padding: "10px 20px"
                         } }
-                        desc="1 - Did not meet expectations, obvious bugs, missing functionality, took longer than expected, etc
+                    >
+                        <MakeInput
+                            type="disabled"
+                            required
+                            value={ `${ this.props.user.firstName } ${ this.props.user.lastName }` }
+                            title="Student"
+                            desc="Your name will be auto filled"
+                        />
+                        <MakeInput
+                            type="suggest"
+                            required
+                            title="What did you study today?"
+                            name="studyToday"
+                            onChange={ select => this.setState( {
+                                studyToday: select
+                            } ) }
+                            value={ this.state.studyToday }
+                            desc="Use search to quickly find the module. Choose the one that matches the page you used in Training Kit"
+                            data={ this.props.lessons &&
+                            Object.values( this.props.lessons )
+                                .map( lesson => lesson ) }
+                        />
+                        <MakeInput
+                            type="disabled"
+                            required
+                            value="¯\_(ツ)_/¯"
+                            title="How are you feeling today?"
+                            desc="Please fill this out on Airtable we don't want to deal with unicode and emojis"
+                        />
+                        <MakeInput
+                            type="input"
+                            required
+                            name="projectSubmittion"
+                            onChange={ this.onChange }
+                            value={ this.state.projectSubmittion }
+                            title="What's the URL for your submission of today's project?"
+                            desc="To submit GitHub Pull Requests's, please include your specific pull request URL"
+                        />
+                        <MakeInput
+                            type="rate"
+                            required
+                            title="On a scale of 1-3, how would you rate your project submission?"
+                            value={ this.state.rateProject }
+                            onChange={ value => {
+                                this.setState( {
+                                    rateProject: value
+                                } );
+                            } }
+                            desc="1 - Did not meet expectations, obvious bugs, missing functionality, took longer than expected, etc
               2 - Met expectations defined in the project in the given time
               3 - Went above and beyond, completed at least one stretch goal, or otherwise added upon the expectations of the project"
-                    />
-                    <MakeInput
-                        type="rate"
-                        required
-                        value={ this.state.rateDay }
-                        onChange={ value => {
-                            this.setState( {
-                                rateDay: value
-                            } );
-                        } }
-                        title="On a scale of 1-3, how would you rate your overall performance and understanding today?"
-                        desc="Consider your attendance, code challenge, project, use of the help channel, participation in live instruction and team meeting."
-                    />
-                    <MakeInput
-                        type="textarea"
-                        title="What did you finish today?"
-                        value={ this.state.finishToday }
-                        name="finishToday"
-                        onChange={ this.onChange }
-                        required
-                    />
-                    <MakeInput
-                        type="textarea"
-                        title="What do you need to finish before tomorrow?"
-                        value={ this.state.finishTommorrow }
-                        name="finishTommorrow"
-                        onChange={ this.onChange }
-                        required
-                    />
-                    <MakeInput
-                        type="textarea"
-                        title="Anything blocking you, or breakthroughs you'd like to share?"
-                        desc="Blockers: what did you try, and what was the error?
+                        />
+                        <MakeInput
+                            type="rate"
+                            required
+                            value={ this.state.rateDay }
+                            onChange={ value => {
+                                this.setState( {
+                                    rateDay: value
+                                } );
+                            } }
+                            title="On a scale of 1-3, how would you rate your overall performance and understanding today?"
+                            desc="Consider your attendance, code challenge, project, use of the help channel, participation in live instruction and team meeting."
+                        />
+                        <MakeInput
+                            type="textarea"
+                            title="What did you finish today?"
+                            value={ this.state.finishToday }
+                            name="finishToday"
+                            onChange={ this.onChange }
+                            required
+                        />
+                        <MakeInput
+                            type="textarea"
+                            title="What do you need to finish before tomorrow?"
+                            value={ this.state.finishTommorrow }
+                            name="finishTommorrow"
+                            onChange={ this.onChange }
+                            required
+                        />
+                        <MakeInput
+                            type="textarea"
+                            title="Anything blocking you, or breakthroughs you'd like to share?"
+                            desc="Blockers: what did you try, and what was the error?
               Breakthroughs: what was your 'a-ha' moment?"
-                        value={ this.state.blockers }
-                        name="blockers"
-                        onChange={ this.onChange }
-                        required
-                    />
-                    <MakeInput
-                        type="textarea"
-                        title="Anything else we should know about?"
-                        value={ this.state.other }
-                        name="other"
-                        onChange={ this.onChange }
-                    />
-                    <Col xs={ 24 } style={ {
-                        margin: "20px 0", textAlign: "center"
-                    } }>
-                        <Button
-                            type="primary"
-                            onClick={ this.submit }
-                            size="large"
-                            icon="link"
-                            loading={ this.state.isSubmitting }
-                        >
-                            Submit
-                        </Button>
-                    </Col>
-                </Row>
-            </Layout.Content>
-        </Layout> );
+                            value={ this.state.blockers }
+                            name="blockers"
+                            onChange={ this.onChange }
+                            required
+                        />
+                        <MakeInput
+                            type="textarea"
+                            title="Anything else we should know about?"
+                            value={ this.state.other }
+                            name="other"
+                            onChange={ this.onChange }
+                        />
+                        <Col xs={ 24 } style={ {
+                            margin: "20px 0", textAlign: "center"
+                        } }>
+                            <Button
+                                type="primary"
+                                onClick={ this.submit }
+                                size="large"
+                                icon="link"
+                                loading={ this.state.isSubmitting }
+                            >
+                                Submit
+                            </Button>
+                        </Col>
+                    </Row>
+                </Layout.Content>
+            </Layout> );
     }
 }
 
--- src/Views/Welcome.jsx
+++ src/Views/Welcome.jsx
@@ -28,66 +28,71 @@ class Welcome extends React.Component{
     };
     
     render(){
-        return ( <Layout>
-            <Layout.Content>
-                <Row
-                    gutter={ 24 }
-                    type="flex"
-                    style={ {
-                        height: "100vh",
-                        margin: "0 30px",
-                        alignItems: "center",
-                        textAlign: "center"
-                    } }
-                >
-                    <Col xs={ 24 } md={ 12 }>
-                        <h1 style={ { fontSize: "2rem" } }>Student
-                            Dashboard</h1>
-                        <h3>
-                            Welcome to the lambda school (unoffical) student
-                            dashboard.
-                        </h3>
-                        <h6>If you don't have a code get it from your
-                            PM</h6>
-                        <Form onSubmit={ this.submit }>
-                            <Form.Item
-                                validateStatus={ this.state.error.status }
-                                help={ this.state.error.msg }
-                            >
-                                <Input
-                                    placeholder="Enter Code"
+        return (
+            
+            <Layout>
+                <Layout.Content>
+                    <Row
+                        gutter={ 24 }
+                        type="flex"
+                        style={ {
+                            height: "100vh",
+                            margin: "0 30px",
+                            alignItems: "center",
+                            textAlign: "center"
+                        } }
+                    >
+                        <Col xs={ 24 } md={ 12 }>
+                            <h1 style={ { fontSize: "2rem" } }>Student
+                                Dashboard</h1>
+                            <h3>
+                                Welcome to the lambda school (unoffical) student
+                                dashboard.
+                            </h3>
+                            <h6>If you don't have a code get it from your
+                                PM</h6>
+                            <Form onSubmit={ this.submit }>
+                                <Form.Item
+                                    validateStatus={ this.state.error.status }
+                                    help={ this.state.error.msg }
+                                >
+                                    <Input
+                                        placeholder="Enter Code"
+                                        size="large"
+                                        onChange={ e => {
+                                            this.setState( {
+                                                code: e.target.value
+                                            } );
+                                        } }
+                                        value={ this.state.code }
+                                        style={ {
+                                            marginBottom: "10px",
+                                            maxWidth: "400px"
+                                        } }
+                                    />
+                                </Form.Item>
+                                <br/>
+                                <Button
+                                    type="primary"
+                                    onClick={ this.submit }
+                                    loading={ this.state.isLoading }
+                                    shape="round"
+                                    icon="lock"
                                     size="large"
-                                    onChange={ e => {
-                                        this.setState( {
-                                            code: e.target.value
-                                        } );
-                                    } }
-                                    value={ this.state.code }
-                                    style={ {
-                                        marginBottom: "10px", maxWidth: "400px"
-                                    } }
-                                />
-                            </Form.Item>
-                            <br/>
-                            <Button
-                                type="primary"
-                                onClick={ this.submit }
-                                loading={ this.state.isLoading }
-                                shape="round"
-                                icon="lock"
-                                size="large"
-                            >
-                                Let's Start
-                            </Button>
-                        </Form>
-                    </Col>
-                    <Col xs={ 24 } md={ 12 }>
-                        <img src={ HomeImage } style={ { width: "100%" } }
-                             alt=""/>
-                    </Col>
-                </Row>
-            </Layout.Content>
-        </Layout> );
+                                >
+                                    Let's Start
+                                </Button>
+                            </Form>
+                        </Col>
+                        
+                        <Col xs={ 24 } md={ 12 }>
+                            <img src={ HomeImage } style={ { width: "100%" } }
+                                 alt=""/>
+                        </Col>
+                    
+                    </Row>
+                </Layout.Content>
+            </Layout> );
     }
 }
 
